13b3e00bec1048d88a53cffe6f57ed9c04d55487
artiskw <81115271+artiskw@users.noreply.github.com>
2022-01-25 09:46:27 -0800

    Update custom.js
    delete, custom.js commenter

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,9 +1,9 @@
 jQuery(function ($) {
     jQuery(document).ready(function () {
         // Mobile menu
-        $(".hamburger").click(function () { //clik funkcija
+        $(".hamburger").click(function () {
             $(this).toggleClass("is-active");
-            $(".page-container").toggleClass("move-right"); //scss kodā raksru, page conatainer iedotu papildus klasi -->page-background<--- un footer
+            $(".page-container").toggleClass("move-right");
         });
     });
 })
